1868ba5a3313da2c1263a47633b5dcd4fccd3898
camerashrimp <232670614+camerashrimp@users.noreply.github.com>
2026-08-25 11:41:50 -0700

    scripting

--- QuickTaskP3Lozano/Assets/Scripts/PlayerMovement.cs
+++ QuickTaskP3Lozano/Assets/Scripts/PlayerMovement.cs
@@ -1,18 +1,20 @@
+using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-   public float speed = 6f;
+   public float speed = 5f;
+    private Rigidbody2D rb;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
